ece3b10b227883d2fa4cdaa608ef72bafa045c49
= <izabeladrabek@Izabelas-MacBook-Pro.local>
2020-05-23 19:28:46 +0200

    DBjazz

--- models/db.php
+++ models/db.php
@@ -24,3 +24,18 @@ public function closeConnection() {
      $this->con = null;
   }
 }
+
+
+class DB {
+    
+    private static $instance = NULL;
+
+    //Singleton Design Pattern
+    public static function getInstance() {
+      if (!isset(self::$instance)) {
+        $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+        self::$instance = new PDO('mysql:host=localhost;dbname=finalproject', 'root', '', $pdo_options);
+      }
+      return self::$instance;
+    }
+}
